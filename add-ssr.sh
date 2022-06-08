1d280ee62b53e7adf048e6dfd00fe0c50331e16e
drixalexa <101800971+drixalexa@users.noreply.github.com>
2022-06-09 03:27:44 +0700

    Update add-ssr.sh

--- add-ssr.sh
+++ add-ssr.sh
@@ -3,7 +3,7 @@ red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- icanhazip.com);
-echo "Script By geo"
+echo "Script By DrixAlexa"
 clear
 IP=$(wget -qO- icanhazip.com);
 echo "Please enter the username you want to set (do not repeat, does not support Chinese, will be reported incorrect!)"
