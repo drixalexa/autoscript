ec89f09949fd3649849219dbdcf13d08e5d5c3ab
drixalexa <101800971+drixalexa@users.noreply.github.com>
2022-06-09 03:23:39 +0700

    Update about.sh

--- about.sh
+++ about.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
 clear
-echo -e "================================================="| lolcat
-echo -e "#         Premium Auto Script By Daorakle       #"| lolcat
-echo -e "#-----------------------------------------------#"| lolcat
-echo -e "# For Debian 9 & Debian 10 64 bit               #"| lolcat
-echo -e "# For Ubuntu 18.04 & Ubuntu 20.04 64 bit        #"| lolcat
-echo -e "# For VPS with KVM and VMWare virtualization    #"| lolcat
-echo -e "# Build Up By Daorakle                          #"| lolcat
-echo -e "#-----------------------------------------------#"| lolcat
+m="\033[0;1;36m"
+y="\033[0;1;37m"
+yy="\033[0;1;32m"
+wh="\033[0m"
+
+clear
+echo -e "$y|===============================================|$wh" 
+echo -e "$y|       Premium Auto Script By DrixAl[SHG]      |$wh" 
+echo -e "$y|-----------------------------------------------|$wh" 
+echo -e "$y| For Debian 9 & Debian 10 64 bit               |$wh" 
+echo -e "$y| For Ubuntu 18.04 & Ubuntu 20.04 64 bit        |$wh" 
+echo -e "$y| For VPS with KVM and VMWare virtualization    |$wh" 
+echo -e "$y| Rebuild By DrixAlexa                          |$wh"
+echo -e "$y|-----------------------------------------------|$wh"
