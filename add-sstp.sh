fccbc555bbd985a7d6560048d00357691476b3c5
drixalexa <101800971+drixalexa@users.noreply.github.com>
2022-06-09 03:28:14 +0700

    Update add-sstp.sh

--- add-sstp.sh
+++ add-sstp.sh
@@ -3,7 +3,7 @@ red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- icanhazip.com);
-echo "Script By geo"
+echo "Script By DrixAlexa"
 clear
 IP=$(wget -qO- icanhazip.com);
 sstp="$(cat ~/log-install.txt | grep -i SSTP | cut -d: -f2|sed 's/ //g')"
